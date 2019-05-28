9f2911d30db12d06d970f5069e8b7a095f2a4e4f
saqibomer <saqibomer.cs@gmail.com>
2019-05-28 12:20:59 +0500

    - Created basic structure

---
+++ test/ViewModels/RecipesViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RecipesViewModel.swift
+//  test
+//
+//  Created by TOxIC on 28/05/2019.
+//  Copyright © 2019 Saqib. All rights reserved.
+//
+
+import UIKit
+
+class RecipesViewModel: NSObject {
+
+}
